e2165412adfd8760c6e79652fa42238aa0b60175
cronossfera <jadrianmorenoj10@gmail.com>
2025-04-24 07:49:35 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -40,7 +40,7 @@ self.addEventListener('install', (event) => {
                 'capsulas.js',
                 'sw.js',
                 'manifest.json',
-                'logo.png',
+                'images/logo.png',
                 'favicon.ico',
                 'sci-fi-bg.jpg',
                 'frutiger-metro-bg.jpg',
